d6520ad0a459c32f39bc7d8352899b6e7fbcac9c
Denzel-Britton <denzel.britton573@gmail.com>
2022-09-23 16:11:12 -0500

    view controller added

---
+++ src/main/java/com/example/moviebackend/controllers/ViewController.java
@@ -0,0 +1,14 @@
+package com.example.moviebackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+//    @RequestMapping({"/", "/about", "/login", "/home", "/posts", "/register", "/me"})
+@RequestMapping({"/","/movies"," ","/searchMovies"})
+    public String showView() {
+        return "forward:/index.html";
+    }
+}
